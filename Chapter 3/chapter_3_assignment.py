c2afc42b6fe27690abb365604a32adf0126ace2f
asahib <39737246+asahib@users.noreply.github.com>
2018-05-30 16:42:34 -0400

    Chapter 3 assignment

---
+++ Chapter 3/chapter_3_assignment.py
@@ -0,0 +1,38 @@
+#For each click variable, calculate the temperature and print it as shown in the instructions
+
+click_1 = 0
+# TODO calculate the temperature, and report it back to the user
+click = click_1 % 50
+temp = click + 40
+print("The latest temperature is %d" % temp)
+
+
+click_2 = 49
+# TODO calculate the temperature, and report it back to the user
+click = click_2 % 50
+temp = click + 40
+print("The latest temperature is %d" % temp)
+
+click_3 = 74
+# TODO calculate the temperature, and report it back to the user
+click = click_3 % 50
+temp = click + 40
+print("The latest temperature is %d" % temp)
+
+click_4 = 51
+# TODO calculate the temperature, and report it back to the user
+click = click_4 % 50
+temp = click + 40
+print("The latest temperature is %d" % temp)
+
+click_5 = -1
+# TODO calculate the temperature, and report it back to the user
+click = click_5 % 50
+temp = click + 40
+print("The latest temperature is %d" % temp)
+
+click_6 = 200
+# TODO calculate the temperature, and report it back to the user
+click = click_6 % 50
+temp = click + 40
+print("The latest temperature is %d" % temp)
