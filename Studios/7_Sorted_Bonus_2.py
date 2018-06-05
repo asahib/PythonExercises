d326db463ca7314e14a43ebd3759b2e4ab144d43
asahib <39737246+asahib@users.noreply.github.com>
2018-06-05 16:46:33 -0400

    Bonus updated

--- Studios/7_Sorted_Bonus_2.py
+++ Studios/7_Sorted_Bonus_2.py
@@ -1,14 +1,21 @@
 def string_pig_latin(str):
   words = str.split()
   new_str = ""
+  vowels = ['a','e','i','o','u']
   for word in words:
-    for vowel in ['a','e','i','o','u']:
-      if word[0] == vowel:
-        first_char = ''
-      else:
-        first_char = word[0]
-    for i in range(len(word)-1):
-      new_str=new_str+word[i+1]
+
+    if word[0] in vowels:
+      first_char = ''
+      j = 0
+      
+    else:
+      first_char = word[0]
+      j = 1
+
+    for i in range(j,len(word)):
+      print(new_str)
+      new_str=new_str+word[i]
+
     new_str = new_str+first_char+"ay "
   return new_str
 
