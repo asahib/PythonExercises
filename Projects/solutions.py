eba163de51cb9149fac7b6a2a1236ddf220e965e
asahib <39737246+asahib@users.noreply.github.com>
2018-06-07 17:14:37 -0400

    Added the name validation to the Initial project

--- Projects/solutions.py
+++ Projects/solutions.py
@@ -5,9 +5,24 @@ def get_initials(full_name):
     initials=f'{initials}{name[0].upper()}'
   return initials
 
+def name_validation(full_name):
+  for i in range(len(full_name)):
+    if(full_name[i]==' '):
+      continue
+    elif(not full_name[i].isalpha()):
+      return False
+  return True
+
+
 def main():
-  fullname = input("What is your full name? \n")
-  print(get_initials(fullname))
+
+  while True:
+    fullname = input("What is your full name? \n")
+    if(name_validation(fullname)):
+      print(get_initials(fullname))
+      break
+    print("\nThe entered name has some invalid characters...\nOnly Alphabets and Spaces are allowed.. Please try again\n")
+    continue
 
 if __name__ == '__main__':
   main()
