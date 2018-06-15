6273a4e70b8e2627dfeaaab7c2579c11169153b5
Sahib <12764179+azharahamed@users.noreply.github.com>
2018-06-15 14:32:51 -0400

    incroporated the bonus instrutctions too.

--- Projects/vigenere.py
+++ Projects/vigenere.py
@@ -16,8 +16,17 @@ def vigenere_encrypt(text, key):
   return ''.join(item for item in new_list)
 
 def main():
-  message = input("Type a message: \n")
-  print(vigenere_encrypt(message, (argv[1])))
+  try:
+    if not argv[1].isalpha():
+      raise ValueError
+    encryption_key = str(argv[1])
+    message = input("Type a message: \n")
+    print(vigenere_encrypt(message, encryption_key))
+  except ValueError:
+    print('usage: python vigenere.py keyword\n       Arguments:\n          -keyword : The string to be used as a "key" to encrypt your message.\n                     Should only contain alphabetic characters-- no numbers or special characters.')
+  except IndexError:
+    print('usage: python vigenere.py keyword\n      Arguments:\n          -keyword : The string to be used as a "key" to encrypt your message.\n                      Should only contain alphabetic characters-- no numbers or special characters.')
+  
 
 if __name__ == '__main__':
   main()
