fd1af5cfe8d51349dfb309048febde87c6935042
Sahib <12764179+azharahamed@users.noreply.github.com>
2018-06-18 17:35:01 -0400

    completed the students studio

--- Studios/student.py
+++ Studios/student.py
@@ -44,14 +44,60 @@ class Student:
   def __repr__(self):
     temp_str = f'\n {self.name} is a {self.classStanding} with GPA of {self.gpa} \n Total Credits : {self.totalcredits} \n Total Grade Points:{self.gradepoints}  '
     return temp_str
+  
+  def getNameandGPA(self):
+    strng = f'{self.name}\t\t\t{self.gpa}'
+    return strng
 
-# class 
+  def getGPA(self):
+    return self.gpa
+
+class Course:
+  def __init__(self, coursename, coursenumber, max_students):
+    self.max_students = max_students
+    self.totalenrolled = 0
+    self.students = []
+
+  def addStudents(self):
+    while True:
+      if self.totalenrolled == self.max_students:
+        print("No more students can't be added! Limit reached")
+        break
+      student_name = input("\nEnter the Student Name : ")  
+      student_id = input("Enter Student Id : ")
+      student = Student(student_name,student_id)
+      self.students.append(student)
+      self.totalenrolled += 1
+
+      Yes_Or_No = input("Please enter 'y' if you want to add another student : ").lower()
+      if Yes_Or_No != 'y':
+        break
+  
+  def getGradeandCredits(self):
+    for i in range(self.totalenrolled):
+      self.students[i].getClassCreditsAndGrade()
+  
+  def classAverageGPA(self):
+    total_GPA = 0
+    for i in range(self.totalenrolled):
+      total_GPA += self.students[i].getGPA()
+    return (total_GPA/self.totalenrolled) 
+
+  def __repr__(self):
+    strng = 'Student Name \t\t GPA\n'
+    for i in range(self.totalenrolled):
+      strng = f"{strng}{self.students[i].getNameandGPA()}\n"
+    return strng
   
 def main():
-  student1 = Student("Azhar","001")
-  student1.getClassCreditsAndGrade()
-  
-  print(student1)
+  # student1 = Student("Azhar","001")
+  # student1.getClassCreditsAndGrade()
+  # print(student1)
+  course1 = Course("Launchcode", '0001', 3)
+  course1.addStudents()
+  course1.getGradeandCredits()
+  print(course1)
+  print(f"The class avarage is {course1.classAverageGPA()}")
 
 if __name__ == '__main__':
   main()
