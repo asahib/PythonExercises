9cd2ec307cadbde19e755001213a347dd47ba1da
azharahamed <12764179+azharahamed@users.noreply.github.com>
2018-07-02 11:50:00 -0400

    optimized solution for primegaps to crack the  codewars

--- coding_challenges/primegaps.py
+++ coding_challenges/primegaps.py
@@ -10,22 +10,31 @@ def isPrime(num):
       return False
   return True
 
-def primeGaps(diff, start, end):
-  initialPrime = 0
-  secondPrime = 0
-  for i in range(start,end+1):
-    if(isPrime(i)):
-      if((i-initialPrime) == diff):
-        secondPrime = i
-        return (initialPrime, secondPrime)
-      else:
-        initialPrime = i
+def gap(diff, start, end):
+  for i in range(start, end+1):
+    if isPrime(i):
+      firstPrime = i
+      for j in range(i+1,end+1):
+        if isPrime(j):
+          if (j-firstPrime == diff):
+            return [firstPrime, j]
+          break
   return None
 
-print(assert_equals(primeGaps(2,100,110),(101,103)))
-print(assert_equals(primeGaps(4,100,110),(103,107)))
-print(assert_equals(primeGaps(6,100,110),None))
-print(assert_equals(primeGaps(8,300,400),(359,367)))
-print(assert_equals(primeGaps(10,300,400),(337,347)))
+
+  '''
+  primes = [item for item in range(start,end+1) if isPrime(item)]
+  for i in range(len(primes)-1):
+    if (primes[i+1] - primes[i]) == diff:
+      return [primes[i],primes[i+1]]
+    elif((primes[i+1] - primes[i]) == diff):
+  '''
+  return None
+
+print(assert_equals(gap(2,100,110),[101,103]))
+print(assert_equals(gap(4,100,110),[103,107]))
+print(assert_equals(gap(6,100,110),None))
+print(assert_equals(gap(8,300,400),[359,367]))
+print(assert_equals(gap(10,300,400),[337,347]))
 
 
